4158a2edd2be11b3ce3b49132e67fd9c0b0daa27
Dominguez <migueldominguezroman01@gmail.com>
2022-10-03 13:32:38 +0200

    a

---
+++ TEMA 1/Ejercicio 7/ejercicio7.js
@@ -0,0 +1,20 @@
+const tableroAjerdrez = (fila) => {
+    if(fila == undefined || fila <= 0|| fila == "" || fila == null || isNaN(fila)){
+        document.body.innerHTML += "Numero de filas no correcto<br/>"
+    }
+
+    let tableroDibujo = "";
+   
+
+    for (let y = 0; y < fila; y++) {
+        for (let x = 0; x < fila; x++) {
+            if ((x + y) % 2 == 0) {
+                tableroDibujo += "■";
+            } else tableroDibujo += "□";
+        }
+        tableroDibujo += "<br>";
+    }
+    document.body.innerHTML += tableroDibujo;
+}
+
+tableroAjerdrez(prompt("Introduce un nuero de filas"))
