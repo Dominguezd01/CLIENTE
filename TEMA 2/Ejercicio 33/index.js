1de343840b25e351055744cac96b93f30f624e55
Dominguez <migueldominguezroman01@gmail.com>
2022-10-25 10:27:55 +0200

    25/10/22

---
+++ TEMA 2/Ejercicio 33/index.js
@@ -0,0 +1,11 @@
+//EJ 33
+
+const getSecondsToday = () => {
+    hoy = new Date();
+
+    mañana = new Date(hoy.getTime() + 8640000)
+    mañana.setHours(0,0,0)
+    return mañana.getTime() - hoy.getTime()/1000
+}
+
+console.log(getSecondsToday())
