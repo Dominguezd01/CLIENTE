83964976556ca4065af38b5618b511eaa269f688
Dominguez <migueldominguezroman01@gmail.com>
2022-10-27 10:15:47 +0200

    27/10/22

--- TEMA 2/Ejercicio 32/index.js
+++ TEMA 2/Ejercicio 32/index.js
@@ -1,2 +1,11 @@
-//EJ 33
+//EJ 32
 
+const getSecondsToday = () => {
+    hoy = new Date();
+
+    mañana = new Date(hoy.getTime() + 8640000)
+    mañana.setHours(0,0,0)
+    return mañana.getTime() - hoy.getTime()/1000
+}
+
+console.log(getSecondsToday())
--- TEMA 2/Ejercicio 33/index.js
+++ TEMA 2/Ejercicio 33/index.js
@@ -1,11 +1,24 @@
-//EJ 33
+/*
+EJERCICIO 33: Segundos que faltan para el día de mañana
+Crea una función getSecondsToTomorrow() que devuelva la cantidad de segundos que faltan para el día de mañana.
 
-const getSecondsToday = () => {
-    hoy = new Date();
+Por ejemplo, si ahora son las 23:00, entonces:
 
-    mañana = new Date(hoy.getTime() + 8640000)
-    mañana.setHours(0,0,0)
-    return mañana.getTime() - hoy.getTime()/1000
+getSecondsToTomorrow() == 3600
+
+
+*/
+
+const getSecondsToTomorrow = () =>{
+    let hoy = new Date()
+
+    let mañana = new Date(2022,9,29)
+
+    console.log(hoy.getTime())
+    console.log(mañana.getTime())
+    let resultado = mañana.getTime()-hoy.getTime()
+
+    console.log(resultado)
 }
 
-console.log(getSecondsToday())
+getSecondsToTomorrow()
