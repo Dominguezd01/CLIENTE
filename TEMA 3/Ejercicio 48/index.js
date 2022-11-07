525f75635c252f7cff459ed06b29106a0527a89f
Dominguez <migueldominguezroman01@gmail.com>
2022-11-07 13:24:46 +0100

    07/11/2022

---
+++ TEMA 3/Ejercicio 48/index.js
@@ -0,0 +1,49 @@
+/*
+Crea una clase Vector que representa un vector. 
+Al crearlo, se pasarán como parámetros dos valores numéricos que serán la x 
+(distancia al punto 0,0 en el eje de las x), y la y 
+((distancia al punto 0,0 en el eje de las y).
+
+Esta clase tendrá dos métodos más: sumar y restar, que toman otro vector como 
+parámetro, y devuelve un nuevo vector resultado de la suma o diferencia de los 
+valores x e y de los dos vectores (el this y el parámetro).
+
+Además, tendrá una propiedad longitud, que devuelve la longitud del vector, 
+esto es, la distancia desde el origen (0,0) al punto (x, y).
+
+console.log(new Vector(1,2).sumar(2, 3)); // => {x: 3, y: 5};
+
+console.log(new Vector(1,2).restar(2, 3)); // => {x: -1, y: -1};
+
+console.log(new Vector(3, 4).longitud); // => 5
+
+*/
+
+class Vector{
+    constructor(x, y){
+        this.x = x 
+        this.y = y
+    }
+
+    sumar(v2){
+        return new Vector(this.x + v2.x, this.y + v2.y)
+        
+    }
+    restar(v2){
+        return new Vector(this.x - v2.x, this.y - v2.y)
+        
+    }
+    longitud(){
+        return Math.sqrt(this.x**2 + this.y**2)
+    }
+}
+
+
+let v1 = new Vector(4,2)
+let v2 = new Vector(4,2)
+console.log(v1.longitud())
+console.log(v1.sumar(v2))
+console.log(v1.restar(v2))
+
+
+console.log(new Vector(2,3).sumar(new Vector(2,3)))
---
+++ TEMA 3/Ejercicio 49/index.js
@@ -0,0 +1,37 @@
+/*
+Crea una clase denominada Vehículo que tenga algunas características como el color, marca, modelo y la velocidad máxima 
+que puede alcanzar.
+
+También tendrá tres métodos además del constructor: comenzar que mostrará el mensaje “Encender motor”,
+ parar que mostrará “Apagar motor” y distanciaMax que recibirá un parámetro con indicando el tiempo de 
+ funcionamiento y calculará la distancia máxima que puede haber recorrido, que será el resultado de multiplicar
+  la velocidad máxima por el tiempo.
+*/
+
+class Vehiculo {
+    constructor(color, marca, modelo, velMax){
+       this.color = color
+       this.marca = marca
+       this.modelo = modelo
+       this.velMax = velMax 
+    }
+    comenzar(){
+        console.log("Encender motor")
+
+    }
+
+    parar(){
+        console.log("El motor se ha parado")
+    }
+
+    distanciaMax(time){
+        return this.velMax * time
+    }
+}
+
+
+let coche1 = new Vehiculo("rojo", "AUDI", "Si", "250")
+
+coche1.comenzar()
+coche1.parar()
+console.log(coche1.distanciaMax(123))
