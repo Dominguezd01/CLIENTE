54948c032d85543108c2e90a491c9463d9772aed
Dominguez <migueldominguezroman01@gmail.com>
2022-11-07 13:03:30 +0100

    07/11/2022

--- TEMA 3/Ejercicio 46/hora.js
+++ TEMA 3/Ejercicio 46/hora.js
@@ -2,10 +2,16 @@ const cambiaHora = () =>{
     let hora = document.getElementById("hora")
     
     let hoy = new Date()
-    if(hoy.getSeconds() < 10){
-        hora.value = `${hoy.getHours()} : ${hoy.getMinutes()} : 0${hoy.getSeconds()}`
+    if(hoy.getMinutes() <= 10){
+        hora.innerHTML = `${hoy.getHours()} : 0${hoy.getMinutes()} : ${hoy.getSeconds()}`
     }else{
-        hora.value = `${hoy.getHours()} : ${hoy.getMinutes()} : ${hoy.getSeconds()}`
+        hora.innerHTML = `${hoy.getHours()} : ${hoy.getMinutes()} : ${hoy.getSeconds()}`
+    }
+    
+    if(hoy.getSeconds() < 10){
+        hora.innerHTML = `${hoy.getHours()} : ${hoy.getMinutes()} : 0${hoy.getSeconds()}`
+    }else{
+        hora.innerHTML = `${hoy.getHours()} : ${hoy.getMinutes()} : ${hoy.getSeconds()}`
     }
 }
 setInterval(cambiaHora, 1000)
