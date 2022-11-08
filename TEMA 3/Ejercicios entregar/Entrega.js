9161e44dd3af321aabbd4de702c7b8f1b11febd3
Dominguezd01 <migueldominguezroman01@gmail.com>
2022-11-08 17:53:22 +0100

    08/11/2022

---
+++ TEMA 3/Ejercicios entregar/Entrega.js
@@ -0,0 +1,18 @@
+class InventarioEmpresa{
+    
+    constructor(elemCode, desc, fechaAlt, fechaBaja){
+        this.elemCode = elemCode
+        this.desc = desc
+        this.fechaAlt = fechaAlt
+        this.fechaBaja = fechaBaja
+    }
+
+    añadir(obj){
+        d
+    }
+    
+}
+
+let n = new InventarioEmpresa(6789, "Cisa", "08-11-2022", "12-12-2022")
+
+console.log(n)
