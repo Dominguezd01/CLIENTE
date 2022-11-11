9644122e88f7947343245b01ed8630ec657aa879
Dominguezd01 <migueldominguezroman01@gmail.com>
2022-11-11 16:01:00 +0100

    11/11/2022

--- TEMA 3/Ejercicios entregar/JS/Inventario.js
+++ TEMA 3/Ejercicios entregar/JS/Inventario.js
@@ -7,20 +7,21 @@ class Articulo{
     }
 
     static añadir(inventario, data){
-        if(inventario.length == 0 || inventario == "undefined"){
-            inventario.push(new Articulo(data.cod, data.desc, new Date().toLocaleDateString())) 
+
+        if(inventario[0] == "" ){
+            inventario[0] = new Articulo(data.cod, data.desc, new Date().toLocaleDateString())
             return inventario
         }else{
             for(let i = 0; i<inventario.length;i++){
-    
-                if(inventario[i].elemCode != data.cod){
-                    console.log(inventario)
-                    inventario.push(new Articulo(data.cod, data.desc, new Date().toLocaleDateString())) 
-                    return inventario
-                }else{
+                if(inventario[i].elemCode === data.cod){
                     let labelAltas = document.getElementById("errorAltas")
                     labelAltas.innerHTML = "Codigo ya existente"
                     labelAltas.style = "display: block; color: red"
+                    return inventario
+                }else{
+                    console.log(inventario)
+                    inventario.push(new Articulo(data.cod, data.desc, new Date().toLocaleDateString())) 
+                    return inventario
                 }
             }
         }     
@@ -30,34 +31,60 @@ class Articulo{
         let rango = inventario.length
         for(let i = 0; i<rango;i++){
             if(inventario[i].elemCode == data){
-                inventario[i].fechaBaja = new Date().toLocaleDateString()
-                console.log(arrayInventario)
-                return inventario
+                if(inventario[i].fechaBaja == undefined){
+                    inventario[i].fechaBaja = new Date().toLocaleDateString()
+                    let labelBajas = document.getElementById("labelBajas")
+                    labelBajas.style = "display: block; color: green"
+                    labelBajas.innerHTML ="Item dado de baja"
+                    return inventario
+                }else{
+                    let labelBajas = document.getElementById("labelBajas")
+                    labelBajas.style = "display: block; color: green"
+                    labelBajas.innerHTML ="Este item ya ha sido dado de baja"
+                }
             }else{
-               let input = document.getElementById("codigoBaja")
-               input.style ="border: solid 1px red"
+               let label = document.getElementById("labelBajas")
+               label.style ="display: block; color:red"
+               label.innerHTML ="Item no encontrado"
                document.getElementById("volverBajas").style = "display: block"
             }
         } 
     } 
     
     static consultarInventario(inventario ){
-        let table = document.getElementById("tablaInventario")
-        table.innerHTML ="<tr><td>Código</td><td>Descripcion</td><td>Fecha alta</td><td>Fecha Baja</td></tr>"
-        inventario.forEach(element => {
-            table.innerHTML += `<td>${element.elemCode}</td><td>${element.desc}</td><td>${element.fechaAlt}</td><td>${element.fechaBaja}</td></tr>`
-        });
-
-        document.getElementById("botonVolverInventario").onclick = ()=>{
-            table.style.display = "none"
-            document.getElementById("h1Inventario").style.display = "none"
-            document.getElementById("formulario1").style.display = "block"
-            document.getElementById("botonVolverInventario").style.display ="none"
+        if(inventario[0] != ""){
+            let table = document.getElementById("tablaInventario")
+            table.style.display ="block"
+            table.innerHTML ="<tr><td>Código</td><td>Descripcion</td><td>Fecha alta</td><td>Fecha Baja</td></tr>"
+            inventario.forEach(element => {
+                table.innerHTML += `<td>${element.elemCode}</td><td>${element.desc}</td><td>${element.fechaAlt}</td><td>${element.fechaBaja}</td></tr>`
+            });
+        }else{
+            let labelInventario =  document.getElementById("errorInvetario")
+            labelInventario.style ="display: block; color:red"
+            labelInventario.innerHTML = "No hay productos en el inventario"
         }
+
     }
 
-    static consultarElemento(inventario, code){
-        return inventario.find(element => element.codeElem == code)
+    static consultarElemento(arrayInventario, code){
+        let devolver = arrayInventario.filter(element => element.elemCode == code)
+        if(devolver.length == 0){
+            return undefined
+        }else{
+            return devolver
+        }
+        
     }
+    static consultarElementoDesc(arrayInventario, desc){
+        let devolver = arrayInventario.filter(element => element.desc == desc)
+        if(devolver.length == 0){
+            return undefined
+        }else{
+            return devolver
+        }
+        
+    }
+  
 }
 
--- TEMA 3/Ejercicios entregar/JS/index.js
+++ TEMA 3/Ejercicios entregar/JS/index.js
@@ -3,13 +3,20 @@ onload = () => {
     document.getElementById("volver").style = "display: none"
 
 }
-var arrayInventario = []
+let arrayInventario = [""]
 document.getElementById("formulario1").addEventListener("submit", e => {
     e.preventDefault()
     const data = Object.fromEntries(new FormData(e.target))
     menus(data)
 })
 
+const mostrarMenu = () =>{
+    document.getElementById("formulario1").style = "display: block"
+}
+
+const ocultarMenu = () =>{
+    document.getElementById("formulario1").style = "display: none"
+}
 const menus = (data) => {
     document.getElementById("formulario1").style.display = "block"
 
@@ -27,6 +34,9 @@ const menus = (data) => {
         case "4":
             mostrarBuscarElemento()
             break
+        case "5":
+            mostrarBuscarDesc()
+            break
 
     }
 }
@@ -34,46 +44,36 @@ const menus = (data) => {
 const menuAltas = () => {
     let formAltas = document.getElementById("formularioAltas")
     formAltas.style.display = "block"
-    document.getElementById("formulario1").style = "display: none"
+    ocultarMenu()
     document.getElementById("formularioAltas").addEventListener("submit", e => {
         e.preventDefault()
-        let labelAltas = document.getElementById("errorAltas")
-        labelAltas.innerHTML = "Codigo ya existente"
-        labelAltas.style = "display: none; color: red"
         const data = Object.fromEntries(new FormData(e.target))
-        console.log(data)
-        let elemCode, desc, fechaAlt;
+        let labelAltas = document.getElementById("errorAltas")
+
+        labelAltas.style = "display: none; color: red"
 
         if (data.cod == "") {
-            changeColor("codigo")
-        } else {
-
-            changeColortoGreen("descripcion")
+            labelAltas.innerHTML = "Introduce los valores correctamente"
+            labelAltas.style.display = "block"
         }
         if (data.desc == "") {
-            changeColor("descripcion")
-        } else {
-            desc = data.desc
-            changeColortoGreen("descripcion")
+            labelAltas.innerHTML = "Introduce los valores correctamente"
+            labelAltas.style.display = "block"
         }
         if (data.cod != "" && data.desc != "") {
             arrayInventario = Articulo.añadir(arrayInventario, data)
-            console.log(arrayInventario)
-            let formAltas = document.getElementById("formularioAltas")
-            formAltas.cod.value= ""
-            formAltas.desc.value= ""
-            formAltas.fechaAlt.value= ""
         }
+        let formAltas = document.getElementById("formularioAltas")
+        formAltas.cod.value= ""
+        formAltas.desc.value= ""
+        formAltas.fechaAlt.value= ""
     })
     document.getElementById("volver").style.display = "block"
 
     document.getElementById("volver").onclick = () => {
-        formAltas.style.display = "none"
-        document.getElementById("formulario1").style.display = "block"
+        formAltas.style.display = "none"    
         document.getElementById("volver").style.display = "none"
-        formAltas.cod.value= ""
-        formAltas.desc.value= ""
-        formAltas.fechaAlt.value= ""
+        mostrarMenu()
     }
 
 }
@@ -92,20 +92,20 @@ const changeColortoGreen = (idElement) => {
 const menuBajas = () => {
     let formBajas = document.getElementById("formularioBajas")
     formBajas.style = "display: block"
-    document.getElementById("formulario1").style.display = "none"
-    document.getElementById("volver").style.display = "none"
+    ocultarMenu()
     let volverBajas = document.getElementById("volverBajas")
     volverBajas.style.display = "block"
     volverBajas.onclick = () => {
         volverBajas.style.display = "none"
         formBajas.style.display = "none"
-        document.getElementById("formulario1").style.display = "block"
+        mostrarMenu()
     }
     formBajas.addEventListener("submit", e => {
         e.preventDefault()
         const data = Object.fromEntries(new FormData(e.target))
-        console.log(data)
+        console.log(data.cod)
         Articulo.darBaja(arrayInventario, data.cod)
+
     })
 } 
 
@@ -113,26 +113,98 @@ const mostrarInventario = () =>{
     let h1Inventario = document.getElementById("h1Inventario")
     let table = document.getElementById("tablaInventario")
     let botonVolverInventario = document.getElementById("botonVolverInventario")
+    ocultarMenu()
     h1Inventario.style.display = "block"
-    table.style.display = "block"
     botonVolverInventario.style.display = "block"
-    
-    document.getElementById("formulario1").style.display = "none"
     Articulo.consultarInventario(arrayInventario)
+
+    document.getElementById("botonVolverInventario").onclick = ()=>{
+        table.style.display = "none"
+        document.getElementById("h1Inventario").style.display = "none"
+        document.getElementById("formulario1").style.display = "block"
+        document.getElementById("botonVolverInventario").style.display ="none"
+        document.getElementById("errorInvetario").style.display ="none"
+    }
 }
 
 const mostrarBuscarElemento = () =>{
     let formulario = document.getElementById("formularioEncontrar")
     formulario.style.display = "block"
-    document.getElementById("formulario1").style.display = "none"
+    let botonVolverEncontrar = document.getElementById("botonVolverEncontrar")
+    botonVolverEncontrar.style.display = "block"
+    botonVolverEncontrar.onclick = () =>{
+        document.getElementById("formulario1").style.display = "block"
+        document.getElementById("volver").style.display = "none"
+        formulario.style.display = "none"
+        botonVolverEncontrar.style.display = "none"
+        document.getElementById("tablaEncontrar").style.display = "none"
+        document.getElementById("errorEncontrar").style = "display:none"
+    }
+    ocultarMenu()
+    formulario.addEventListener("submit", e =>{
+        e.preventDefault()
+        const data = Object.fromEntries(new FormData(e.target)) 
+        const devuelto = Articulo.consultarElemento(arrayInventario,data.cod)
+        formulario.cod.value=""
+        if(devuelto == undefined){
+            let etiqueta = document.getElementById("errorEncontrar")
+            etiqueta.style = "display:block; color:red"
+            etiqueta.innerHTML ="Codigo no encontrado"
+        }else{
+            let etiqueta = document.getElementById("errorEncontrar")
+            etiqueta.style.display = "none"
+            let tablaEncontrar = document.getElementById("tablaEncontrar")
+            tablaEncontrar.style.display = "block"
+            tablaEncontrar.innerHTML += `<tr><td>${devuelto[0].elemCode}</td>
+                                            <td>${devuelto[0].desc}</td>
+                                            <td>${devuelto[0].fechaAlt}</td>
+                                            <td>${devuelto[0].fechaBaja}</td>`
+        }
+
+
+    })
+
+}
+
+const mostrarBuscarDesc = () =>{
+    let formulario = document.getElementById("formularioEncontrarDesc")
+    formulario.style.display = "block"
+    let botonVolverEncontrar = document.getElementById("botonVolverEncontrarDesc")
+    botonVolverEncontrar.style.display = "block"
+    botonVolverEncontrar.onclick = () =>{
+        document.getElementById("formulario1").style.display = "block"
+        document.getElementById("volver").style.display = "none"
+        formulario.style.display = "none"
+        botonVolverEncontrar.style.display = "none"
+        document.getElementById("tablaEncontrarDesc").style.display = "none"
+        document.getElementById("errorEncontrarDesc").style = "display:none"
+    }
+    ocultarMenu()
     formulario.addEventListener("submit", e =>{
         e.preventDefault()
         const data = Object.fromEntries(new FormData(e.target))
-        console.log(data)
-        const devuelto =  Articulo.consultarElemento(data)
+        const devuelto = Articulo.consultarElementoDesc(arrayInventario,data.cod)
+        console.log(devuelto)
+        formulario.cod.value=""
+        if(devuelto == undefined){
+            let etiqueta = document.getElementById("errorEncontrarDesc")
+            etiqueta.style = "display:block; color:red"
+            etiqueta.innerHTML ="Descripcion no encontrada"
+        }else{
+            let etiqueta = document.getElementById("errorEncontrarDesc")
+            etiqueta.style.display = "none"
+            let tablaEncontrar = document.getElementById("tablaEncontrarDesc")
+            tablaEncontrar.style.display = "block"
+            tablaEncontrar.innerHTML += `<tr><td>${devuelto[0].elemCode}</td>
+                                            <td>${devuelto[0].desc}</td>
+                                            <td>${devuelto[0].fechaAlt}</td>
+                                            <td>${devuelto[0].fechaBaja}</td>`
+        }
 
-        let tablaEncontrar = document.getElementById("tablaEncontrar")
-        tablaEncontrar.innerHTML += `<td>${devuelto.elemCode}`
+
     })
+
 }
+
+
 
